f869f2a0dcc3ad3319ea1ff99875c622581e79a5
mmohamedirfan8015-pixel <mmohamedirfan8015@gmail.com>
2026-06-27 11:22:03 +0530

    Refactor vehicle class and implement inheritance

--- vehicle.java
+++ vehicle.java
@@ -1,5 +1,27 @@
-class vehicle {
-    void 
+class Vehicle {
+    String brand = "Toyota";
 
-    
+    void start() {
+        System.out.println("Vehicle is starting...");
+    }
+}
+
+
+class Car extends Vehicle {
+    String model = "Corolla";
+
+    void display() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+    }
+}
+
+// Main class
+public class SingleInheritanceDemo {
+    public static void main(String[] args) {
+        Car car = new Car();
+
+        car.start();     
+        car.display();    
+    }
 }
