3cbbb70e1491c8e615697fa7983bef5c4cfab197
Mohamed Irfan <mmohamedirfan8015@gmail.com>
2026-06-27 12:06:10 +0530

    git commit -m "Fixed class declaration

--- MultilevelInheritanceDemo.java
+++ MultilevelInheritanceDemo.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Grandfather {
     void land() {
         System.out.println("Grandfather owns land.");
@@ -18,7 +17,7 @@ class Son extends Father {
     }
 }
 
-// Main class
+
 public class MultilevelInheritanceDemo {
     public static void main(String[] args) {
         Son s = new Son();
@@ -28,34 +27,5 @@ public class MultilevelInheritanceDemo {
         s.car();    
     }
 }
-=======
-class Grandfather {
-    void land() {
-        System.out.println("Grandfather owns land.");
-    }
-}
-
-class Father extends Grandfather {
-    void house() {
-        System.out.println("Father owns a house.");
-    }
-}
 
 
-class Son extends Father {
-    void car() {
-        System.out.println("Son owns a car.");
-    }
-}
-
-// Main class
-public class MultilevelInheritanceDemo {
-    public static void main(String[] args) {
-        Son s = new Son();
-
-        s.land(); 
-        s.house();  
-        s.car();    
-    }
-}
->>>>>>> 16701b80978a014c9747d5153c4423c97ea410d8
