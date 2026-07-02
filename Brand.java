e9449cac626a0e92cf845e59b4f7ac30432967a6
Mohamed Irfan <mmohamedirfan8015@gmail.com>
2026-07-02 11:45:29 +0530

    update

---
+++ Brand.java
@@ -0,0 +1,16 @@
+class Mobile{
+    String brand="Samsung";
+    class Battery{
+        void show(){
+            System.out.println("Battery belongs to"+brand);
+        }
+    }
+}
+public class Brand {
+    public static void main(String[] args){
+        Mobile m=new Mobile();
+        Mobile.Battery b=m.new Battery();
+        b.show();
+    }
+    
+}
---
+++ InnerClass.java
@@ -0,0 +1,14 @@
+class Car{
+    static String company="BMW";
+    static class Engine{
+        void start(){
+            System.out.println("Engine Started");
+            System.out.println("Company:"+company);
+        }
+    }
+}
+public class InnerClass {
+    public static void main(String[] args){
+            
+    }   
+}
