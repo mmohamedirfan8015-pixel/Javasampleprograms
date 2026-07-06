09139b3548e3b131d8d6570cfdc4748e531eb712
Mohamed Irfan <mmohamedirfan8015@gmail.com>
2026-07-06 10:24:17 +0530

    update

---
+++ files/FileExistsDemo.java
@@ -0,0 +1,14 @@
+import java.io.File;
+
+public class FileExistsDemo {
+    public static void main(String[] args) {
+
+        File file = new File("sample.txt");
+
+        if (file.exists()) {
+            System.out.println("File exists");
+        } else {
+            System.out.println("File does not exist");
+        }
+    }
+}
