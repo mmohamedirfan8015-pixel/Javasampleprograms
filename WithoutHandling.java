d7bd8f15625159f38c39b165d9367997ee4a4f04
Mohamed Irfan <mmohamedirfan8015@gmail.com>
2026-07-11 10:18:17 +0530

    update

--- WithoutHandling.java
+++ WithoutHandling.java
@@ -1,13 +1,26 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+
 public class WithoutHandling {
-    public static void main(String[] args){
-        try{
-        FileReader file=new FileReader("data.txt");
-        System.out.println("File Opened Successfully");
-        } catch(FileNotFoundException e){
+    public static void main(String[] args) {
+
+        FileReader file = null;
+
+        try {
+            file = new FileReader("data.txt");
+            System.out.println("File Opened Successfully");
+        } catch (FileNotFoundException e) {
             System.out.println("File Not Found");
+        } finally {
+            if (file != null) {
+                try {
+                    file.close();
+                    System.out.println("File Closed Successfully");
+                } catch (IOException e) {
+                    System.out.println("Error while closing file");
+                }
+            }
         }
-        file.close();
     }
 }
