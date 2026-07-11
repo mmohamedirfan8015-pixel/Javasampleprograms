a10890b48723d66e9c2d3fe23e08b2eca2ae3d89
Mohamed Irfan <mmohamedirfan8015@gmail.com>
2026-07-11 09:52:05 +0530

    update

--- WithoutHandling.java
+++ WithoutHandling.java
@@ -1,12 +1,13 @@
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 public class WithoutHandling {
     public static void main(String[] args){
         try{
         FileReader file=new FileReader("data.txt");
         System.out.println("File Opened Successfully");
-        } catch(filenotfoundexpeption e){
+        } catch(FileNotFoundException e){
             System.out.println("File Not Found");
         }
-        
+        file.close();
     }
 }
---
+++ comparable.java
@@ -0,0 +1,13 @@
+class Student {
+    int id;
+    String name;
+    Student(int id,String name)
+    {
+        this.id=id;
+        this.name=name;
+    }
+    @Override
+    public String toString(){
+        return id+" "+name+" ";
+    }
+}
