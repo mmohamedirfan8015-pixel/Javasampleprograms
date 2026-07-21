7f5280e67103ba60b7fad8b0633c6c299321982a
Mohamed Irfan <mmohamedirfan8015@gmail.com>
2026-07-21 17:33:30 +0530

    update

---
+++ BinaryTree.java
@@ -0,0 +1,58 @@
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+public class BinaryTree {
+
+    Node root;
+    void preorder(Node node) {
+        if (node == null)
+            return;
+
+        System.out.print(node.data + " ");
+        preorder(node.left);
+        preorder(node.right);
+    }
+    void inorder(Node node) {
+        if (node == null)
+            return;
+
+        inorder(node.left);
+        System.out.print(node.data + " ");
+        inorder(node.right);
+    }
+    void postorder(Node node) {
+        if (node == null)
+            return;
+
+        postorder(node.left);
+        postorder(node.right);
+        System.out.print(node.data + " ");
+    }
+
+    public static void main(String[] args) {
+
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+
+        System.out.print("Preorder: ");
+        tree.preorder(tree.root);
+
+        System.out.print("\nInorder: ");
+        tree.inorder(tree.root);
+
+        System.out.print("\nPostorder: ");
+        tree.postorder(tree.root);
+    }
+}
